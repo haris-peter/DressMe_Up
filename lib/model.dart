e580ff3cb81cf2adbd9840877ff7880f4f130068
Grace Laptop <gracethakkolkaran29@gmail.com>
2023-11-12 19:52:45 +0530

    changes

---
+++ lib/model.dart
@@ -0,0 +1,7 @@
+class User {
+  String uid;
+  String email;
+  String displayName;
+
+  User({required this.uid, required this.email, required this.displayName});
+}
---
+++ lib/sign_in.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignInPage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        await _auth.signInWithCredential(credential);
+      } else {
+        // Handle the case where the user canceled the sign-in.
+        print('Google Sign-In canceled.');
+      }
+    } catch (e) {
+      print('Error signing in with Google: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _signInWithGoogle,
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
